fd4e485b26a32329232e3b622656f94bccf4a9b5
DavidSotir <sotir.davidm@gmail.com>
2016-06-14 00:53:59 -0500

    Unity Ads Test Finished.

---
+++ Unity Ads Test/Unity Ads Test/Assets/Scripts/Ads.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+
+	string adID = "video";
+
+	public void ShowAds()
+	{
+		ShowOptions options = new ShowOptions();
+		options.resultCallback = AdsCallback;
+
+		Advertisement.Show(adID, options);
+    }
+
+	void AdsCallback(ShowResult result)
+	{
+		if (result == ShowResult.Finished)
+		{
+			Debug.Log("Ad Finished!");
+			//give reward
+		}
+		else if (result == ShowResult.Failed)
+		{
+			Debug.Log("Ad Failed!");
+		}
+		else if (result == ShowResult.Skipped)
+		{
+			Debug.Log("Ad Skipped!");
+		}
+	}
+}
